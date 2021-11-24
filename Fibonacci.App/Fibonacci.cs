34465b92e5b52b3ee2b5f0ff4b329edf4fa7d0ec
Igor Kind <86460417+ngessda@users.noreply.github.com>
2021-11-24 12:08:01 +0900

    none

---
+++ Fibonacci.App/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci.App
+{
+    class Fibonacci
+    {
+        private int index;
+        private int result;
+        public Fibonacci(int ix)
+        {
+            index = ix;
+        }
+        public int FibonacciNumberValue()
+        {
+            result = FibonacciNumberFinder(index);
+            return result;
+        }
+
+        private int FibonacciNumberFinder(int ix)
+        {
+            if (ix < 1) 
+            {
+                return 0;
+            }
+            if (ix == 1) 
+            {
+                return 1;
+            }
+            return FibonacciNumberFinder(ix - 1) + FibonacciNumberFinder(ix - 2);
+        }
+    }
+}
---
+++ Fibonacci.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fib = new Fibonacci(9);
+            Console.WriteLine(fib.FibonacciNumberValue());
+            Console.ReadKey();
+        }
+    }
+}
